7bea0021fe7e90ffe76e9e324930e69e27cf4ee0
Hugo A Guerra <guerrahugoa@gmail.com>
2016-03-19 21:51:57 -0700

    included preliminary s3 functionality

--- routes/route-handle.js
+++ routes/route-handle.js
@@ -1,8 +1,16 @@
 'use strict';
+var AWS = require('aws-sdk');
+AWS.config.region = 'us-west-2';
+var s3 = new AWS.S3();  //revise
+var params = {Bucket: 'user-file-bucket', Key: } //revise
+var fs = require('fs');
+
+
 
 module.exports = (middleRouter, models) => {
   let User = models.User;
   let File = models.File;
+
   middleRouter.route('/users')
   .get((req, res) => {
     console.log('GET route hit for /users');
@@ -17,6 +25,8 @@ module.exports = (middleRouter, models) => {
       res.json(user);
     });
   });
+
+  //routes for users/:user
 
   middleRouter.route('/users/:user')
   .get((req, res) => {
@@ -41,13 +51,39 @@ module.exports = (middleRouter, models) => {
     })
   });
 
+  //routes for users/:user/files
+
   middleRouter.route('/users/:user/files')
   .get((req, res) => {
     console.log('GET route hit for /users/:user/files');
+    User.findById(req.params.id, (err, user) => {
+      res.json(user.files);
+    })
+
   })
   .post((req, res) => {
     console.log('POST route hit for /users/:user/files');
+    fs.writeFile('./data/' + req.body.fileName, req.body.content, (err) => {
+      console.log('file written as ./data/' + req.body.fileName + ' with content: ' + req.body.content);
+    });
+    var params = {Bucket: 'user-file-bucket', Key: req.body.fileName, Body: req.body.content};
+    s3.putObject(params, (err, data) => {
+      if (err) console.log(err);
+      console.log('successfully sent data to s3. data is: ' + data);
+    });
+    var url = s3.getSignedUrl('objectName', params, (err, url) => { //revise objectName, find actual name
+      console.log('url is ' + url); //revise, maybe put all new file and push to user in here. if so, get rid of var.
+    })
+    var newFile = new File(req.body);
+    newFile.save((err, file) => {
+      User.findById(req.params.id, (err, user) => {
+        user.files.push(file._id); //test
+      });
+      res.json(url); //change
+    });
   });
+
+  //routes for /files/:file
 
   middleRouter.route('/files/:file')
   .get((req, res) => {
