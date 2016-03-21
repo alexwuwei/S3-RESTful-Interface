80b5c6b5c692e58e81b02c2d6c83cb69a3b603da
Hugo A Guerra <guerrahugoa@gmail.com>
2016-03-21 11:23:39 -0700

    initialized tests, modified files/:file route

--- test/test.js
+++ test/test.js
@@ -8,3 +8,29 @@ var http = require('http');
 require('../server.js');
 var User = require('../models/users-model');
 var File = require('../models/files-model');
+
+describe('testing routes for /users resource', () => {
+  it('should hit a GET route for /customers', (done) => {
+    request('localhost:3000')
+    .get('/users')
+    .end((err, res) => {
+      expect(err).to.equal(null);
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a('object');
+      done();
+    });
+  });
+  it('should hit a POST route for /users', (done) => {
+    request('localhost:3000')
+    .post('/users')
+    .send({"name":"testPerson"})
+    .end((err, res) => {
+      expect(err).to.equal(null);
+      expect(res).to.have.status(200);
+      expect(res).to.be.a('object');
+      expect(res.body).to.have.property('name');
+      done();
+    });
+  });
+});
